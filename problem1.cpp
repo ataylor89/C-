04b8b812000c547c2d3d19754acd5c386b137ce5
Andrew Taylor <rolyata47@gmail.com>
2022-01-29 17:33:22 -0600

    Substituting tabs with four spaces

--- problem1.cpp
+++ problem1.cpp
@@ -18,29 +18,29 @@ public:
 };
 
 int main(int argc, char** argv) {
-	Solution solution;
-	vector<int> nums;
-	int sum;
-	char *line;
-	char *token;
-	int num;	
-	
-	cout << "Input a comma-separated list of numbers with no spaces:" << endl;
-	cout << "Comma-separated list: ";
-	cin >> line;
+    Solution solution;
+    vector<int> nums;
+    int sum;
+    char *line;
+    char *token;
+    int num;    
+    
+    cout << "Input a comma-separated list of numbers with no spaces:" << endl;
+    cout << "Comma-separated list: ";
+    cin >> line;
 
-	token = strtok(line, ",");
-	while (token != NULL) {
-		num = stoi(token, nullptr, 10);
-		nums.push_back(num);
-		token = strtok(NULL, ",");	
-	}
+    token = strtok(line, ",");
+    while (token != NULL) {
+        num = stoi(token, nullptr, 10);
+        nums.push_back(num);
+        token = strtok(NULL, ",");    
+    }
 
-	cout << "Input a desired sum: ";
-	cin >> line;
-	sum = stoi(line, nullptr, 10);
+    cout << "Input a desired sum: ";
+    cin >> line;
+    sum = stoi(line, nullptr, 10);
 
-	vector<int> indices = solution.twoSum(nums, sum);
-	cout << "Indices: " << indices[0] << " and " << indices[1] << endl;
-	return 0;
+    vector<int> indices = solution.twoSum(nums, sum);
+    cout << "Indices: " << indices[0] << " and " << indices[1] << endl;
+    return 0;
 }
--- problem2.cpp
+++ problem2.cpp
@@ -3,76 +3,76 @@
 using namespace std;
 
 struct ListNode {
-	int val;
-	ListNode *next;
-	ListNode() : val(0), next(nullptr) {}
-	ListNode(int x) : val(x), next(nullptr) {}
-	ListNode(int x, ListNode *next) : val(x), next(next) {}
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
 };
 
 class Solution {
 public:
-	ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-		ListNode *head = new ListNode;
-		ListNode *tail = head;
-		
-		int sum, digit, carry = 0;
-		while (l1 || l2 || carry > 0) {
-			// cout << "carry = " << carry << endl;
-			sum = carry;
-			if (l1) {
-				sum += l1->val;
-				// cout << "l1->val = " << l1->val << endl;
-				l1 = l1->next;
-			}
-			if (l2) {
-				sum += l2->val;
-				// cout << "l2->val = " << l2->val << endl;
-				l2 = l2->next;
-			}
-			// cout << "sum = " << sum << endl;
-			digit = sum % 10;
-			// cout << "digit = " << digit << endl;
-			tail->val = digit;
-			carry = sum / 10;
-			if (l1 || l2 || carry > 0) {
-				tail->next = new ListNode;
-				tail = tail->next;
-			}
-			// cout << endl;
-		}
-		return head;
-	}
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        ListNode *head = new ListNode;
+        ListNode *tail = head;
+        
+        int sum, digit, carry = 0;
+        while (l1 || l2 || carry > 0) {
+            // cout << "carry = " << carry << endl;
+            sum = carry;
+            if (l1) {
+                sum += l1->val;
+                // cout << "l1->val = " << l1->val << endl;
+                l1 = l1->next;
+            }
+            if (l2) {
+                sum += l2->val;
+                // cout << "l2->val = " << l2->val << endl;
+                l2 = l2->next;
+            }
+            // cout << "sum = " << sum << endl;
+            digit = sum % 10;
+            // cout << "digit = " << digit << endl;
+            tail->val = digit;
+            carry = sum / 10;
+            if (l1 || l2 || carry > 0) {
+                tail->next = new ListNode;
+                tail = tail->next;
+            }
+            // cout << endl;
+        }
+        return head;
+    }
 };
 
 ListNode* linked_list(int num) {
-	ListNode *head = new ListNode;
-	ListNode *tail = head;
-	while (num > 0) {
-		tail->val = num % 10;
-		num /= 10;
-		if (num > 0) {
-			tail->next = new ListNode;
-			tail = tail->next;
-		}
-	}
-	return head;
+    ListNode *head = new ListNode;
+    ListNode *tail = head;
+    while (num > 0) {
+        tail->val = num % 10;
+        num /= 10;
+        if (num > 0) {
+            tail->next = new ListNode;
+            tail = tail->next;
+        }
+    }
+    return head;
 }
 
 int main(int argc, char **argv) {
-	ListNode *l1 = linked_list(243);
-	ListNode *l2 = linked_list(564);
+    ListNode *l1 = linked_list(243);
+    ListNode *l2 = linked_list(564);
 
-	Solution solution;
-	ListNode *sum = solution.addTwoNumbers(l1, l2);
+    Solution solution;
+    ListNode *sum = solution.addTwoNumbers(l1, l2);
 
-	string result;
-	while (sum) {
-		result += to_string(sum->val);
-		sum = sum->next;
-	}
-	reverse(result.begin(), result.end());
+    string result;
+    while (sum) {
+        result += to_string(sum->val);
+        sum = sum->next;
+    }
+    reverse(result.begin(), result.end());
 
-	cout << "Sum: " << result << endl;
-	return 0;
+    cout << "Sum: " << result << endl;
+    return 0;
 }
