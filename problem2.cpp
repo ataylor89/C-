ffdbf8614d2fb1e7cd184d1827886be1a1128250
Andrew Taylor <rolyata47@gmail.com>
2022-01-29 19:44:24 -0600

    Added standard IO to problem2.cpp

--- problem2.cpp
+++ problem2.cpp
@@ -3,76 +3,89 @@
 using namespace std;
 
 struct ListNode {
-    int val;
-    ListNode *next;
-    ListNode() : val(0), next(nullptr) {}
-    ListNode(int x) : val(x), next(nullptr) {}
-    ListNode(int x, ListNode *next) : val(x), next(next) {}
+	int val;
+	ListNode *next;
+	ListNode() : val(0), next(nullptr) {}
+	ListNode(int x) : val(x), next(nullptr) {}
+	ListNode(int x, ListNode *next) : val(x), next(next) {}
 };
 
 class Solution {
 public:
-    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        ListNode *head = new ListNode;
-        ListNode *tail = head;
-        
-        int sum, digit, carry = 0;
-        while (l1 || l2 || carry > 0) {
-            // cout << "carry = " << carry << endl;
-            sum = carry;
-            if (l1) {
-                sum += l1->val;
-                // cout << "l1->val = " << l1->val << endl;
-                l1 = l1->next;
-            }
-            if (l2) {
-                sum += l2->val;
-                // cout << "l2->val = " << l2->val << endl;
-                l2 = l2->next;
-            }
-            // cout << "sum = " << sum << endl;
-            digit = sum % 10;
-            // cout << "digit = " << digit << endl;
-            tail->val = digit;
-            carry = sum / 10;
-            if (l1 || l2 || carry > 0) {
-                tail->next = new ListNode;
-                tail = tail->next;
-            }
-            // cout << endl;
-        }
-        return head;
-    }
+	ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+		ListNode *head = new ListNode;
+		ListNode *tail = head;
+		
+		int sum, digit, carry = 0;
+		while (l1 || l2 || carry > 0) {
+			// cout << "carry = " << carry << endl;
+			sum = carry;
+			if (l1) {
+				sum += l1->val;
+				// cout << "l1->val = " << l1->val << endl;
+				l1 = l1->next;
+			}
+			if (l2) {
+				sum += l2->val;
+				// cout << "l2->val = " << l2->val << endl;
+				l2 = l2->next;
+			}
+			// cout << "sum = " << sum << endl;
+			digit = sum % 10;
+			// cout << "digit = " << digit << endl;
+			tail->val = digit;
+			carry = sum / 10;
+			if (l1 || l2 || carry > 0) {
+				tail->next = new ListNode;
+				tail = tail->next;
+			}
+			// cout << endl;
+		}
+		return head;
+	}
 };
 
 ListNode* linked_list(int num) {
-    ListNode *head = new ListNode;
-    ListNode *tail = head;
-    while (num > 0) {
-        tail->val = num % 10;
-        num /= 10;
-        if (num > 0) {
-            tail->next = new ListNode;
-            tail = tail->next;
-        }
-    }
-    return head;
+	ListNode *head = new ListNode;
+	ListNode *tail = head;
+	while (num > 0) {
+		tail->val = num % 10;
+		num /= 10;
+		if (num > 0) {
+			tail->next = new ListNode;
+			tail = tail->next;
+		}
+	}
+	return head;
+}
+
+string to_string(ListNode* node) {
+	string result;
+	while (node) {
+		result += to_string(node->val);
+		node = node->next;
+	}
+	reverse(result.begin(), result.end());
+	return result;
 }
 
 int main(int argc, char **argv) {
-    ListNode *l1 = linked_list(243);
-    ListNode *l2 = linked_list(564);
+	Solution solution;
+	ListNode *sum = new ListNode;
+	int n;
+	while (cin >> n) {
+		ListNode *node = linked_list(n);
+		sum = solution.addTwoNumbers(sum, node);	
+		string result = to_string(sum);
+		cout << "Sum: " << result << endl;
+	}	
 
-    Solution solution;
-    ListNode *sum = solution.addTwoNumbers(l1, l2);
+	// ListNode *l1 = linked_list(243);
+	// ListNode *l2 = linked_list(564);
 
-    string result;
-    while (sum) {
-        result += to_string(sum->val);
-        sum = sum->next;
-    }
-    reverse(result.begin(), result.end());
+	// Solution solution;
+	// ListNode *sum = solution.addTwoNumbers(l1, l2);
 
-    cout << "Sum: " << result << endl;
-    return 0;
+	// cout << "Sum: " << result << endl;
+	return 0;
 }
